a1a8cfc117329829f44be250faa073098e1e1be7
Maxence Mathieu <Sans.Plague@gmail.com>
2021-11-12 22:17:26 +0100

    added an exception and changed voice language

--- StTtS.py
+++ StTtS.py
@@ -1,17 +1,29 @@
 from gtts import gTTS
 import os
 import sys
-import playsound
+import time
+from pygame import mixer
 import speech_recognition as sr
 import keyboard
 
 def speak(text):
-    tts = gTTS(text=text, lang='fr')
+	tts = gTTS(text=text, lang='en-US')
 
-    filename = "abc.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
-    os.remove(filename)
+	path = root_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+	filename = "abc.mp3"
+	path = os.path.join(root_dir, filename)
+
+	tts.save(path) #Save mp3 file
+
+	mixer.init()
+	mixer.music.load(path)
+	mixer.music.play() #Play the mp3 file
+
+	while mixer.music.get_busy():  #Wait for music to finish playing
+		time.sleep(0.1)
+
+	mixer.music.unload() #Unload music to delete the file, or else it thows an error
+	os.remove(path)
 
 
 r = sr.Recognizer()
@@ -27,15 +39,20 @@ Key = input("Quelle touche du clavier active la reconaissance vocale : ")
 while True:  # making a loop
 	try:  # used try so that if user pressed other than the given key error will not be shown
 		if keyboard.is_pressed(Key):  # if key 'q' is pressed 
-			with Microphone as source:
+			with Microphone as source: 
+				r.adjust_for_ambient_noise(source)
 				print("Speak!")
-				audio_data = r.listen(source)
-			result = r.recognize_google(audio_data, language="fr-FR")
+				audio_data = r.listen(source) #Get the voice of the user
+
+			result = r.recognize_google(audio_data, language='en-US') #Send the voice to google, and get text result (might throw a UnknownValueError exception)
 			print (">", result)
 			print("\n")
-			speak(result)
-	except:
+			speak(result) 
+	except sr.UnknownValueError: #If google doesn't recognize speach
+		print("Google n'a pas reconnu ce que vous avez dit, désolé \n")
+
+	except: #Should never happen ...
+		print()
 		print("Oops!", sys.exc_info()[0], "occurred.")
 		temp = input("Press enter to exit ....")
 		break
-
